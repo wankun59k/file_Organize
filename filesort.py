7f7f274ed782fc8d715c07db17c834cd2c688c93
wankun59k <wankun59k@gmail.com>
2021-10-09 16:17:10 +0900

    v1

--- filesort.py
+++ filesort.py
@@ -1,26 +1,36 @@
-import pathlib
 import glob
 import os
 import asyncio
 import aioshutil
+import configparser
+import shutil
 
-path = 'F:/**/(成年コミック)*.*'
-dst_path = pathlib.Path('F:\HEN_MANGA\SEIKOMI')
-rm_path = pathlib.Path('F:\remove')
 
+config_ini = configparser.ConfigParser()
+config_ini.read('config.ini', encoding='utf-8')
+
+path = config_ini['DEFAULT'].get('path')
+dst_path = config_ini['DEFAULT'].get('dst_path')
+rm_path = config_ini['DEFAULT'].get('rm_path')
 
 tasks = []
-
+moved = []
+duplicated = []
 async def move_glob(tgt_path):
-    if not os.path.exists(os.path.join(dst_path, os.path.basename(tgt_path))):
+    checkpath = os.path.join(dst_path, os.path.basename(tgt_path))
+    if not checkpath in moved:
+        moved.append(checkpath)
         await aioshutil.move(tgt_path, dst_path)
     else:
+        if checkpath in duplicated:
+            shutil.remove(tgt_path)
+        duplicated.append(checkpath)
         await aioshutil.move(tgt_path, rm_path)
-        await aioshutil.rmtree(rm_path)
 
 async def main():
     for p in glob.glob(path, recursive=True):
         tasks.append(asyncio.create_task(move_glob(p)))
+        #print(p)
     
     print('start program')
 
@@ -28,3 +38,8 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+    print("{} files are duplicate.".format(len(duplicated)))
+    for file in duplicated:
+        print("> {} duplicate.".format(file))
+    for file in moved:
+        print("> {} moved.".format(file))
